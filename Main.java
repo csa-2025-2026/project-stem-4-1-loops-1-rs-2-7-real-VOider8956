707471800e1e55c4fe18ba53fc3de06d2992f485
VOider8956 <99462247+VOider8956@users.noreply.github.com>
2025-10-17 21:46:23 +0000

    67676676767567676766776776

--- Main.java
+++ Main.java
@@ -4,6 +4,20 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    //Problem 1
+    Scanner summing = new Scanner(System.in);
+    System.out.println("Please begin typing out numbers, each one will be added up (Use -1 to stop)");
+    int lah = summing.nextInt();
+    int total = 0;
+
+    while (lah > 1) {
+      total += lah;
+      lah = summing.nextInt();
+    }
+    System.out.println("Sum is " + total );
+    
+    //Problem 2
+    Scanner howmany = newScanner(System.in);
+    System.out.println("How many numbers would you like to add up in total ")
   }
 }
